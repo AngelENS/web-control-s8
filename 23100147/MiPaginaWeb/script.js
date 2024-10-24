3351b8ecaa72894bb3f886bba2c33c9a1e876d3f
Marck-Code1 <marco.celis2005@gmail.com>
2024-10-24 17:03:36 -0500

    Nueva pagina web

---
+++ 23100147/MiPaginaWeb/script.js
@@ -0,0 +1,14 @@
+
+const paragraph = document.getElementById('pe');
+
+function cambiarColor(){
+document.body.style.backgroundColor = 'cadetblue'
+}
+function cambiarColor2(){
+document.body.style.backgroundColor = 'gray'
+}
+function cambiarColor3(){
+		document.body.style.backgroundColor = 'black'
+		alert("MODO SERIO ACTIVADO!!!")
+}
+
---
+++ 23100147/script.js
@@ -0,0 +1,10 @@
+const boton = document.getElementById('cambiar-color');
+
+function cambiarColor(){
+document.body.style.backgroundColor = 'pink'
+}
+
+
+
+
+boton.addEventListener('click', () => {document.body.style.backgroundColor = 'pink'})
