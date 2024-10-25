e3e03c186c17e4ee5583a49b0531e16ce5d53827
shij0-zip <josueal.cass@gmail.com>
2024-10-25 08:36:46 -0600

    primer version

---
+++ 23100144/script.js
@@ -0,0 +1,9 @@
+const boton = document.getElementById('cambiar-color');
+
+//function cambiarColor(){
+//   document.body.style.backgroundColor='pink';
+//}
+
+boton.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'pink';
+})
