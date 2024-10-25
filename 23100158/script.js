3c87023cdadc5b1cf59c8880eed4b394b3b5d336
Tavo-ux <alejandrocoto928@gmail.com>
2024-10-24 23:29:53 -0500

    Mi practica de mi pagina web

---
+++ 23100158/script.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById('Cambiar-color');
+//function cambiarColor()
+  //  {
+    //    document.body.style.backgroundColor = 'pink';
+    //}      
+boton.addEventListener('click', ()=> { document.body.style.backgroundColor = 'pink';})
